b6fce19c5e6ca7f3b7ec33e9effbf657ebf1b0ba
Joyson <cj4dev@gmail.com>
2025-01-10 22:50:57 +0530

    cj commit

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../Context/AppContext'
 
 const Doctors = () => {
+
+  const {speciality} = useParams()
+  const { doctors } = useContext(AppContext)
+  const [filterDoc, setFilterDoc] = useState([])
+
   return (
-    <div></div>
+    <div>
+      <p>Browse through the doctors specialist.</p>
+      <div>
+        <div>
+          <p>General physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
