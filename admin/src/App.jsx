3d164d9a39d36febe200aca355b7806efb6c1e03
Joyson <cj4dev@gmail.com>
2025-04-11 02:15:43 +0530

    Completed adding a doctor logic

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,13 +2,31 @@ import React, { useContext } from "react";
 import LoginAdmin from "./pages/LoginAdmin";
 import { ToastContainer, toast } from "react-toastify";
 import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllApointments from "./pages/Admin/AllApointments";
+import AddDoctor from "./pages/Admin/AddDoctor.jsx";
+import DoctorsList from "./pages/Admin/DoctorsList";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
 
   return aToken ? (
-    <div>
+    <div className="bg-[#F8F9FD]">
       <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllApointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
